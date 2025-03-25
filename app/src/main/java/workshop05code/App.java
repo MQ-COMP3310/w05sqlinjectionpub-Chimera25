a9409c761a2721296a831b15d593397a55126957
Chimera25 <c.muscat22@outlook.com>
2025-03-25 14:01:51 +1100

    Providing alerts for invalid word

--- app/src/main/java/workshop05code/App.java
+++ app/src/main/java/workshop05code/App.java
@@ -76,6 +76,11 @@ public class App {
             while (!guess.equals("q")) {
                 System.out.println("You've guessed '" + guess+"'.");
 
+                if((!guess.matches("[a-z]")) || guess.length() > 4){
+                    System.out.println("This is not acceptible input, please try again.\n");
+
+                }
+
                 if (wordleDatabaseConnection.isValidWord(guess)) { 
                     System.out.println("Success! It is in the the list.\n");
                 }else{
@@ -85,6 +90,7 @@ public class App {
                 System.out.print("Enter a 4 letter word for a guess or q to quit: " );
                 guess = scanner.nextLine();
             }
+            
         } catch (NoSuchElementException | IllegalStateException e) {
             e.printStackTrace();
         }
